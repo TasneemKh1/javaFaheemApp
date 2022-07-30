b3248573ba6ef43d5e8c6a36de1328827fdfd9e1
Tasneem13 <khraistasneem@gmail.com>
2022-07-30 23:49:15 +0300

    first commit

---
+++ src/test/java/appiumTest/faheem.java
@@ -0,0 +1,55 @@
+package appiumTest;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class faheem extends base{
+	public static void main(String[] args) throws MalformedURLException {
+		AndroidDriver <AndroidElement> driver=Capabilities();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+
+		//check logo is visible 
+		WebElement Logo=driver.findElementByClassName("android.widget.ImageView");
+		System.out.println("logo is displayed: "+Logo.isDisplayed());
+		Assert.assertTrue(Logo.isDisplayed());
+		
+		
+
+		//check the text equals "تصفح كزائر" and click on link 
+		WebElement BrowseAsVisitor=driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.TextView");
+		System.out.println("Link is displayed: "+BrowseAsVisitor.getText());
+		boolean isEnabled = BrowseAsVisitor.isEnabled();
+		Assert.assertEquals(isEnabled, true);
+		Assert.assertEquals("تصفح كزائر",BrowseAsVisitor.getText());
+		BrowseAsVisitor.click();
+
+		//check the page is present in DOM and visible
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup")));
+		WebElement Page=driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup");
+		Assert.assertTrue(Page.isDisplayed());
+
+		// check the "مرحلتك الدراسية" header is displayed
+		WebElement textHeader=driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.widget.TextView");
+		Assert.assertTrue(textHeader.isDisplayed());
+		Assert.assertEquals(textHeader.getText(), "مرحلتك الدراسية");
+
+		//check the number of buttons equals 13
+		System.out.println("size "+driver.findElementsByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup").size());
+		Assert.assertEquals(driver.findElementsByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup").size(), 13);
+
+
+	}
+
+
+
+}
